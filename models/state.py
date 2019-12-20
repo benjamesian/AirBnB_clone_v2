ae75622a7b7daeced95b6042d1864057f0fff049
Ben Cook <808@holbertonschool.com>
2019-12-19 21:59:49 -0800

    update city and state classes to work with SQLAlchemy

--- models/state.py
+++ models/state.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
+    
+    if os.getenv('HBNB_TYPE_STORAGE') == 'file':
+        @property
+        def cities(self):
+            obs = {}
+            for k, v in models.storage.all(models.City).items():
+                if v.state_id == self.id:
+                    obs[k] = v
+            return obs
