1c2c7b9611a3c526d103ce8adac25fb00a3b9874
maxastuart <maxastuart@gmail.com>
2019-12-21 05:57:42 +0000

    cat!!!!!!

--- tests/test_models/test_engine/test_db.py
+++ tests/test_models/test_engine/test_db.py
@@ -22,6 +22,16 @@ class TestDBStorage(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """set up for test"""
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
         cls.user = User()
         cls.user.first_name = "Kev"
         cls.user.last_name = "Yo"
