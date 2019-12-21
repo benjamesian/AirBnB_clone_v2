9318b919e7ee0e3d740cec6ecddc8c8af32240ca
maxastuart <maxastuart@gmail.com>
2019-12-21 00:04:25 +0000

    fix typo

--- models/amenity.py
+++ models/amenity.py
@@ -15,5 +15,5 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
     place_amenities = relationship('Place',
-                                   secondary='place_amenities',
+                                   secondary='place_amenity',
                                    back_populates='amenities')
