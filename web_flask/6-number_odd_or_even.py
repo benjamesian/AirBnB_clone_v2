c4a646bc8464ef5c8900ce28f9c50ff5932ebfd8
Ben Cook <808@holbertonschool.com>
2020-01-21 16:50:41 -0800

    fix pep8 style

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -37,8 +37,8 @@ def number_template(n):
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-    odd_or_even = 'even' if n % 2 == 0 else 'odd'
-    return render_template('6-number_odd_or_even.html', n=n, odd_or_even=odd_or_even)
+    oe = 'even' if n % 2 == 0 else 'odd'
+    return render_template('6-number_odd_or_even.html', n=n, odd_or_even=oe)
 
 
 if __name__ == '__main__':
